55e5748dcc3aa20e180f9fd1bdc481f996801bcd
montseliz <montse.liz92@gmail.com>
2022-12-09 15:06:30 +0100

    tasca_s1_04

--- s1_4_niv3_montseliz/src/test/java/s1_4_niv3_montseliz/AppTest_ex6.java
+++ s1_4_niv3_montseliz/src/test/java/s1_4_niv3_montseliz/AppTest_ex6.java
@@ -2,6 +2,7 @@ package s1_4_niv3_montseliz;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.Test;
 
 public class AppTest_ex6 {
@@ -10,8 +11,21 @@ public class AppTest_ex6 {
 	
 	@Test
 	public void exceptionTest() {
+		
+		assertThatExceptionOfType(ArrayIndexOutOfBoundsException.class).isThrownBy(new ThrowingCallable() {
+			
+			@Override
+			public void call() throws ArrayIndexOutOfBoundsException {
+				System.out.println(values[4]);
+			}
+		});
+	}
+	
+	/*@Test
+	public void exceptionTest() {
+		
 		Throwable thrown = catchThrowable(() -> System.out.println(values[4]));
 		assertThat(thrown).isInstanceOf(ArrayIndexOutOfBoundsException.class);
-
-	}
+		
+	}*/
 }
